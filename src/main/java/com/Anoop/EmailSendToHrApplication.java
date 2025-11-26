082952e0e26eab2803170c5f6062dccb921bb08e
Anoop kumar <anoopsaw91025@gmail.com>
2025-11-26 15:09:00 +0530

    Project Structure created

---
+++ src/main/java/com/Anoop/EmailSendToHrApplication.java
@@ -0,0 +1,13 @@
+package com.Anoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSendToHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSendToHrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Anoop/EmailSendToHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Anoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSendToHrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
